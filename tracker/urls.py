f437a746239e794af8b06fea13cae375e475a37c
amj133 <amj@vt.edu>
2018-05-10 18:09:36 -0600

    add url path and view method for /api/v1/foods

--- tracker/urls.py
+++ tracker/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url('api/v1/foods', views.foods),
-    url('', views.index),
+    url(r'^api/v1/foods/$', views.get_post_foods, name='get_post_foods'),
+    # url('api/v1/foods', views.foods),
+    # url('', views.index),
 ]
--- tracker/views.py
+++ tracker/views.py
@@ -1,3 +1,8 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Food
+from .serializers import FoodSerializer
 from django.http import HttpResponse
 
 def index(request):
@@ -6,3 +11,26 @@ def index(request):
 def foods(request):
     response = "You're looking at the foods index."
     return HttpResponse(response)
+
+@api_view(['GET', 'DELETE', 'PATCH'])
+def get_delete_update_food(request, pk):
+    try:
+        food = Food.objects.get(pk=pk)
+    except Food.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        return Response({})
+    elif request.method == 'DELETE':
+        return Response({})
+    elif request.method == 'PATCH':
+        return Response({})
+
+@api_view(['GET', 'POST'])
+def get_post_foods(request):
+    if request.method == 'GET':
+        foods = Food.objects.all()
+        serialized = FoodSerializer(foods, many=True)
+        return Response(serialized.data)
+    elif request.method == 'POST':
+        return Response({})
