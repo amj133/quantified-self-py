0d98e13c8497d792b0f04352465af1d2f25351bb
amj133 <amj@vt.edu>
2018-05-11 16:59:27 -0600

    write test creates_food

--- tracker/tests/test_views.py
+++ tracker/tests/test_views.py
@@ -58,3 +58,61 @@ class GetSingleFoodTest(TestCase):
         response = client.get(reverse('get_delete_update_food', kwargs={'pk': 4}))
 
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+class CreateFoodTest(TestCase):
+
+    def setUp(self):
+        self.valid_payload = {
+            "food": {
+                "name": "Mint",
+                "calories": "225"
+            }
+        }
+        self.invalid_payload_1 = {
+            "food" : {
+                "name": "Mint"
+            }
+        }
+        self.invalid_payload_2 = {
+            "food" : {
+                "calories": "225"
+            }
+        }
+
+    def test_creates_food(self):
+        response = client.post(
+            reverse('get_post_foods'),
+            data=json.dumps(self.valid_payload),
+            content_type='application/json'
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    # def test_does_not_create_food_with_no_calories(self):
+    #     response = client.post(
+    #         reverse('get_post_foods'),
+    #         data=json.dumps(self.invalid_payload_1),
+    #         content_type='application/json'
+    #     )
+    #
+    #     self.assertEqual(response.status_code.HTTP_400_BAD_REQUEST)
+    #
+    # def test_does_not_create_food_with_no_name(self):
+    #     response = client.post(
+    #         reverse('get_post_foods'),
+    #         data=json.dumps(self.invalid_payload_2),
+    #         content_type='application/json'
+    #     )
+    #
+    #     self.assertEqual(response.status_code.HTTP_400_BAD_REQUEST)
+
+# class EditFoodTest(TestCase):
+#
+#     def setUp(self):
+#         Food.objects.create(
+#             name='Banana',
+#             calories=140
+#         )
+#
+#     def test_edits_food(self):
+#         response = client.patch(reverse('get_delete_update_food', kwargs={'pk': 1}))
