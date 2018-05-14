43171bc05c77da95c23d4b79c29ea5a32ca0a472
amj133 <amj@vt.edu>
2018-05-14 11:11:41 -0600

    create test_views_meals with get all meals test

---
+++ tracker/tests/test_views_meals.py
@@ -0,0 +1,42 @@
+import json
+from rest_framework import status
+from django.test import TestCase, Client
+from django.urls import reverse
+from ..models import Food
+from ..models import Meal
+# from ..serializers import FoodSerializer
+from ..serializers import MealSerializer
+import code
+
+client = Client()
+
+class GetAllMealsTest(TestCase):
+
+    def setUp(self):
+        breakfast = Meal.objects.create(name='Breakfast')
+        lunch = Meal.objects.create(name='Lunch')
+        banana = Food.objects.create(
+            name='Banana',
+            calories=140
+        )
+        twizzler = Food.objects.create(
+            name='Twizzler',
+            calories=240
+        )
+        avocado = Food.objects.create(
+            name='Avocado',
+            calories=340
+        )
+        breakfast.foods.add(banana)
+        breakfast.foods.add(twizzler)
+        lunch.foods.add(avocado)
+
+
+    def test_get_all_meals(self):
+        response = client.get(reverse('get_meals'))
+
+        meals = Meal.objects.all()
+        serialized = MealSerializer(meals, many=True)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, serialized.data)
