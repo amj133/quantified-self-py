01246679e77d0a07056bb56a2042928c7962f57f
amj133 <amj@vt.edu>
2018-05-19 16:39:32 -0600

    remove dj database url

--- quantified_self_py/settings.py
+++ quantified_self_py/settings.py
@@ -146,6 +146,6 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+# import dj_database_url
+# db_from_env = dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(db_from_env)
